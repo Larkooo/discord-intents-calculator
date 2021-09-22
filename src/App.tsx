722a79c51a500cbeb5a6f60c5b8765f2ce299922
Nasr DJ <59736843+Larkooo@users.noreply.github.com>
2021-09-21 20:13:20 -0400

    updated to latest available intents and fixed calculation bug

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,11 @@ function App() {
     "READY",
     "RESUMED",
     "VOICE_SERVER_UPDATE",
-    "USER_UPDATE"
+    "USER_UPDATE",
+    "APPLICATION_COMMAND_CREATE",
+    "APPLICATION_COMMAND_UPDATE",
+    "APPLICATION_COMMAND_DELETE",
+    "INTERACTION_CREATE"
   ]
 
   const intents : intentsInterface = {
@@ -64,63 +68,91 @@ function App() {
       "CHANNEL_CREATE",
       "CHANNEL_UPDATE",
       "CHANNEL_DELETE",
-      "CHANNEL_PINS_UPDATE"
-    ], 
+      "CHANNEL_PINS_UPDATE",
+      "THREAD_CREATE",
+      "THREAD_UPDATE",
+      "THREAD_DELETE",
+      "THREAD_LIST_SYNC",
+      "THREAD_MEMBER_UPDATE",
+      "THREAD_MEMBERS_UPDATE *",
+      "STAGE_INSTANCE_CREATE",
+      "STAGE_INSTANCE_UPDATE",
+      "STAGE_INSTANCE_DELETE"
+    ],
+
     GUILD_MEMBERS: [
       "GUILD_MEMBER_ADD",
       "GUILD_MEMBER_UPDATE",
-      "GUILD_MEMBER_REMOVE"
+      "GUILD_MEMBER_REMOVE",
+      "THREAD_MEMBERS_UPDATE *",
     ], 
+
     GUILD_BANS: [
       "GUILD_BAN_ADD",
-      "GUILD_BAN_REMOVE"
-    ], 
-    GUILD_EMOJIS: [
-      "GUILD_EMOJIS_UPDATE"
-    ], 
+      "GUILD_BAN_REMOVE",
+    ],
+
+    GUILD_EMOJIS_AND_STICKERS: [
+      "GUILD_EMOJIS_UPDATE",
+      "GUILD_STICKERS_UPDATE",
+    ],
+
     GUILD_INTEGRATIONS: [
-      "GUILD_INTEGRATIONS_UPDATE"
-    ], 
+      "GUILD_INTEGRATIONS_UPDATE",
+      "INTEGRATION_CREATE",
+      "INTEGRATION_UPDATE",
+      "INTEGRATION_DELETE",
+    ],
+
     GUILD_WEBHOOKS: [
       "WEBHOOKS_UPDATE"
-    ], 
+    ],
+
     GUILD_INVITES: [
       "INVITE_CREATE",
-      "INVITE_DELETE"
-    ], 
+      "INVITE_DELETE",
+    ],
+
     GUILD_VOICE_STATES: [
       "VOICE_STATE_UPDATE"
-    ], 
+    ],
+
     GUILD_PRESENCES: [
       "PRESENCE_UPDATE"
-    ], 
+    ],
+
     GUILD_MESSAGES: [
       "MESSAGE_CREATE",
       "MESSAGE_UPDATE",
       "MESSAGE_DELETE",
-      "MESSAGE_DELETE_BULK"
-    ], 
+      "MESSAGE_DELETE_BULK",
+    ],
+
     GUILD_MESSAGE_REACTIONS: [
       "MESSAGE_REACTION_ADD",
       "MESSAGE_REACTION_REMOVE",
       "MESSAGE_REACTION_REMOVE_ALL",
-      "MESSAGE_REACTION_REMOVE_EMOJI"
-    ], 
+      "MESSAGE_REACTION_REMOVE_EMOJI",
+    ],
+
     GUILD_MESSAGE_TYPING: [
       "TYPING_START"
-    ], 
+    ],
+
     DIRECT_MESSAGES: [
       "MESSAGE_CREATE",
       "MESSAGE_UPDATE",
       "MESSAGE_DELETE",
-      "CHANNEL_PINS_UPDATE"
-    ], 
+      "CHANNEL_PINS_UPDATE",
+    ],
+
     DIRECT_MESSAGE_REACTIONS: [
       "MESSAGE_REACTION_ADD",
       "MESSAGE_REACTION_REMOVE",
       "MESSAGE_REACTION_REMOVE_ALL",
-      "MESSAGE_REACTION_REMOVE_EMOJI"
-    ], 
+      "MESSAGE_REACTION_REMOVE_EMOJI",
+    ],
+
     DIRECT_MESSAGE_TYPING: [
       "TYPING_START"
     ]
@@ -131,7 +163,7 @@ function App() {
     GUILDS: false,
     GUILD_MEMBERS: false,
     GUILD_BANS: false,
-    GUILD_EMOJIS: false,
+    GUILD_EMOJIS_AND_STICKERS: false,
     GUILD_INTEGRATIONS: false,
     GUILD_WEBHOOKS: false,
     GUILD_INVITES: false,
@@ -148,19 +180,19 @@ function App() {
       guildMembers: localStorage.getItem('guildMembersIntent') === 'true' || false
     },
     intents: 0,
-    eventsCount: 4
+    eventsCount: basicIntents.length
   });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
-    let intentsValue;
-    let eventsCountValue;
+    let intentsValue = state.intents;
+    let eventsCountValue = state.eventsCount;
     if(event.target.name !== "presence" && event.target.name !== "guildMembers") {
       if(event.target.checked) { 
-        intentsValue = state.intents += 1 << Object.keys(state).indexOf(event.target.name)
-        eventsCountValue = state.eventsCount+intents[event.target.name].length
+        intentsValue += 1 << (Object.keys(state).indexOf(event.target.name) - 1)
+        eventsCountValue += intents[event.target.name].length
       } else {
-        intentsValue = state.intents -= 1 << Object.keys(state).indexOf(event.target.name) 
-        eventsCountValue = state.eventsCount-intents[event.target.name].length
+        intentsValue -= 1 << (Object.keys(state).indexOf(event.target.name) - 1)
+        eventsCountValue -= intents[event.target.name].length
       }
       setState({ ...state, [event.target.name]: event.target.checked, intents: intentsValue, eventsCount: eventsCountValue});
     } else {
@@ -316,7 +348,7 @@ function App() {
       <br/>
       <Typography variant='caption'>Click on this button to copy</Typography>
 
-      <p style={{color: 'gray'}}>Copyright © Aymen Djeghmoum - Larko#1783</p>      
+      <p style={{color: 'gray'}}>Copyright © Nasr AA Djeghmoum - Larko#0742 - <a href="https://github.com/Larkooo/discord-intents-calculator">Github</a></p>      
     </Container>
     </ThemeProvider>
     
